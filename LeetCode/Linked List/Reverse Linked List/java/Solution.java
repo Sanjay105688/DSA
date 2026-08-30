9e4fa0444d2f6fd149a7daf973ca9b38dfabcd91
SANJAY P <sanjaypalanisamy774@gmail.com>
2026-08-30 11:06:50 +0530

    feat: Add Reverse Linked List

--- LeetCode/Linked List/Reverse Linked List/java/Solution.java
+++ LeetCode/Linked List/Reverse Linked List/java/Solution.java
@@ -1,23 +1,13 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev=null;
-        ListNode curr= head;
-        while(curr!=null){
-            ListNode temp=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=temp;
-        }
-        return prev;
+    ListNode prev=null;
+    ListNode curr=head;
+    while(curr!=null){
+        ListNode  temp=curr.next;    //step1
+        curr.next=prev;              //step2
+         prev = curr;                //step3
+        curr=temp;                   //step4
+    }
+    return prev;
     }
 }
